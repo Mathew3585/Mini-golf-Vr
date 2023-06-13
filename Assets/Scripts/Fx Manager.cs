7b6704d066c690d88fa3f839069834a595932401
Mathew-Simon <mathew.simon2004@gmail.com>
2023-06-13 16:21:06 +0200

    Update Shop

--- Assets/Scripts/Fx Manager.cs
+++ Assets/Scripts/Fx Manager.cs
@@ -18,7 +18,13 @@ public class FxManager : MonoBehaviour
 
     [Space(10)]
     [Header("FX Goal")]
-    public GameObject FxGoalConfetie;
+    public GameObject FxGoalConfetieWhite;
+    public GameObject FxGoalConfetieRed;
+    public GameObject FxGoalConfetieBlue;
+    public GameObject FxGoalConfetieGreen;
+    public GameObject FxGoalConfetieYellow;
+    public GameObject FxGoalConfetieViolet;
+    public GameObject FxGoalConfetieRgb;
 
 
     private void Awake()
